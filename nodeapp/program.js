56aba182685d320258689b0a59a67da87aec5ede
Jonas Jerndin <jonas@jerndin.se>
2018-01-11 15:44:26 +0100

    Added test code for the event tracing.

--- nodeapp/program.js
+++ nodeapp/program.js
@@ -1,6 +1,44 @@
 'use strict';
 
+
 var http = require('http');
+
+var bodyString = JSON.stringify({
+    channelId: 'default',
+    eventType: 'NOdeAppStarted',
+    category: 'Category'
+});
+
+var headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': bodyString.length
+};
+
+var options = {
+  host: 'transienteventdisplay20180111081216.azurewebsites.net',
+  path: '/api/events',
+  port: 80,
+  method: 'POST',
+headers: headers
+};
+
+var callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function(chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function() {
+  console.log(str);
+  });
+};
+
+http.request(options, callback).write(bodyString);
+
+
 
 var server = http.createServer(function (req, res) {
     
